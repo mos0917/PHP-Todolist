913d49f573313166f6aa2a358a389042b9c202a7
MOS09 <shonan.fireball@gmail.com>
2020-06-05 10:00:54 +0900

    configエラーのため修正

--- core/config.php
+++ core/config.php
@@ -3,6 +3,6 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
-$password = $url["pass"]
-$db = substr($url["path"], 1);
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
 
