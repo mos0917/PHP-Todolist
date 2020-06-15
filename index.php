a9de34ea7b80c99b69a316e2ad7cf737ffad0a2c
MOS09 <shonan.fireball@gmail.com>
2020-06-15 15:44:40 +0900

    script修正

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if (isset($_POST['method']) && ($_POST['method'] === 'put')) {
 <title>Todo List</title>
 </head>
 <body>
-<script src="todoscript.js">
+<script src="todoscript.js"></script>
 <div class="container">
 <h1>Todo List</h1>
 <input class="form-control" type="text" placeholder="以下にに「タスク名」、「内容」を入力し、「登録」ボタンを押下してください。" readonly>
@@ -180,6 +180,6 @@ echo '</dl>';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
-</script>
+
 </body>
 </html>
