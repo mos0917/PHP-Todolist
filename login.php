7b65c1f986f57659fc5e3e6208c6a3d629132b02
MOS09 <shonan.fireball@gmail.com>
2020-06-17 20:55:05 +0900

    ボタン位置修正

--- login.php
+++ login.php
@@ -2,40 +2,42 @@
 ob_start();
 // ここから、register.phpと同様
 session_start();
-if( isset($_SESSION['user']) != "") {
-	header("Location: index.php");
+if (isset($_SESSION['user']) != '') {
+    header('Location: index.php');
 }
 include_once 'dbconnect.php';
 // ここまで、register.phpと同様
 ?>
 
 <?php
-if(isset($_POST['login'])) {
-	$email = $mysqli->real_escape_string($_POST['email']);
-	$password = $mysqli->real_escape_string($_POST['password']);
-	// クエリの実行
-	$query = "SELECT * FROM users WHERE email='$email'";
-	$result = $mysqli->query($query);
-	if (!$result) {
-		print('クエリーが失敗しました。' . $mysqli->error);
-		$mysqli->close();
-		exit();
-	}
-	// パスワード(暗号化済み）とユーザーIDの取り出し
-	while ($row = $result->fetch_assoc()) {
-		$db_hashed_pwd = $row['password'];
-		$user_id = $row['user_id'];
-	}
-	// データベースの切断
-	$result->close();
-	// ハッシュ化されたパスワードがマッチするかどうかを確認
-	if (password_verify($password, $db_hashed_pwd)) {
-		$_SESSION['user'] = $user_id;
-		header("Location: index.php");
-		exit;
-	} else { ?>
+if (isset($_POST['login'])) {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $password = $mysqli->real_escape_string($_POST['password']);
+    // クエリの実行
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $result = $mysqli->query($query);
+    if (!$result) {
+        echo 'クエリーが失敗しました。'.$mysqli->error;
+        $mysqli->close();
+        exit();
+    }
+    // パスワード(暗号化済み）とユーザーIDの取り出し
+    while ($row = $result->fetch_assoc()) {
+        $db_hashed_pwd = $row['password'];
+        $user_id = $row['user_id'];
+    }
+    // データベースの切断
+    $result->close();
+    // ハッシュ化されたパスワードがマッチするかどうかを確認
+    if (password_verify($password, $db_hashed_pwd)) {
+        $_SESSION['user'] = $user_id;
+        header('Location: index.php');
+        exit;
+    } else {
+        ?>
 		<div class="alert alert-danger" role="alert">メールアドレスとパスワードが一致しません。</div>
-	<?php }
+	<?php
+    }
 } ?>
 
 <!DOCTYPE HTML>
@@ -50,32 +52,37 @@ if(isset($_POST['login'])) {
 </head>
 </head>
 <body>
-<div class="col-xs-6 col-xs-offset-3">
+<div class="container">
 
 <?php
 // signupがPOSTされたときに下記を実行
-if(isset($_POST['signup'])) {
-	$username = $mysqli->real_escape_string($_POST['username']);
-	$email = $mysqli->real_escape_string($_POST['email']);
-	$password = $mysqli->real_escape_string($_POST['password']);
-	$password = password_hash($password, PASSWORD_BCRYPT);
-	// POSTされた情報をDBに格納する
-	$query = "INSERT INTO users(username,email,password) VALUES('$username','$email','$password')";
-	if($mysqli->query($query)) {  ?>
+if (isset($_POST['signup'])) {
+    $username = $mysqli->real_escape_string($_POST['username']);
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $password = $mysqli->real_escape_string($_POST['password']);
+    $password = password_hash($password, PASSWORD_BCRYPT);
+    // POSTされた情報をDBに格納する
+    $query = "INSERT INTO users(username,email,password) VALUES('$username','$email','$password')";
+    if ($mysqli->query($query)) {
+        ?>
 		<div class="alert alert-success" role="alert">登録しました</div>
-		<?php } else { ?>
+		<?php
+    } else {
+        ?>
 		<div class="alert alert-danger" role="alert">エラーが発生しました。</div>
 		<?php
-	}
+    }
 } ?>
 
 <form method="post">
 	<h1>Todo List ログイン</h1>
-	<div class="form-group">
-		<input type="email"  class="form-control" id="exampleInputEmail1" name="email" placeholder="メールアドレス" required />
-	</div>
-	<div class="form-group">
-		<input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="パスワード" required />
+	<div class="row">
+		<div class="col-md-12">
+			<input type="email"  class="form-control" id="exampleInputEmail1" name="email" placeholder="メールアドレス" required />
+		</div>
+		<div class="col-md-12">
+			<input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="パスワード" required />
+		</div>
 	</div>
 	<button type="submit" class="btn btn-primary" name="login">ログインする</button>
 	<a href="register.php">会員登録はこちら</a>
