04279f56cc15812db193b221783b716f041d7d78
MOS09 <shonan.fireball@gmail.com>
2020-06-20 21:32:19 +0900

    一旦昔のVerに戻し

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if (isset($_POST['submit'])) { //登録ボタン押下時の処理
     }
 }
 
-if (isset($_POST['method']) && ($_POST['method'] === 'put')) { //「完了」ボタン押下時に以下の処理を実行
+if (isset($_POST['method']) && ($_POST['method'] === 'put')) {
     $id = $_POST['id'];
     $id = htmlspecialchars($id, ENT_QUOTES);
     $id = (int) $id;
@@ -83,9 +83,6 @@ if (isset($_POST['method']) && ($_POST['method'] === 'put')) { //「完了」ボ
 <html>
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-
 <meta charset="utf-8">
 <meta name="viewport" content="width=width=device-width, initial-scale=1">
 <title>Todo List</title>
@@ -95,10 +92,10 @@ if (isset($_POST['method']) && ($_POST['method'] === 'put')) { //「完了」ボ
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-6 col-lg-9">
+        <div class="col-xs-9 col-lg-9">
             <h1>Todo List</h1>
         </div>
-        <div class="col-xs-6 col-lg-3">
+        <div class="col-xs-3 col-lg-3">
             <form action="index.php" method="post" onsubmit="return logoutChk()">
                     <input class="btn btn-outline-primary" type="button" name="logout" onclick="location.href='./logout.php?logout'"  value="ログアウト">
             </form>
@@ -109,7 +106,7 @@ if (isset($_POST['method']) && ($_POST['method'] === 'put')) { //「完了」ボ
             <input class="form-control" type="text" placeholder="以下に「タスク名」、「内容」を入力し、「登録」ボタンを押下してください。" readonly>
         </div>
     </div>
-    <span>■ログイン中のユーザー：<?php echo $username; ?> さん</span>
+<span>■ログイン中のユーザー：<?php echo $username; ?> さん</span>
 
 
     <form action="index.php" method="post" onsubmit="return errChk();">
@@ -175,50 +172,13 @@ while ($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo '</dd>';
 
     echo $task['deadline_date'];
-    //以下、編集モーダルを表示させる処理AND完了ボタン処理
+
     echo '<dd>';
     echo '
-            <div class="modal fade" id="edittask'.$task['id'].'" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="label1">Todo編集画面</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col-xs-12 col-lg-6">
-                                    <ul>
-                                        <li>
-                                            <span>タスク名</span>
-                                            <input type="text" class="form-control" id="edittaskname" name="editname" value="'.$task['name'].'">
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-xs-12 col-lg-6">
-                                    <ul>
-                                        <li>
-                                            <span>内容</span>
-                                            <textarea class="form-control" id="edittaskvalue" rows="1" name="editmemo">'.$task['memo'].'</textarea>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
-                            <button type="button" class="btn btn-primary">更新</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edittask'.$task['id'].'">編集</button>
-
             <form action="index.php" method="post">
             <input type="hidden" name="method" value="put">
-            <input type="hidden" name="task_id" value="'.$task['id'].'">
+            <input type="hidden" name="id" value="'.$task['id'].'">
+            <button type="show" class="btn btn-warning" >編集</button>
             <button type="show" class="btn btn-danger" >完了</button>
             </form>
           ';
@@ -233,8 +193,9 @@ echo '</dl>';
 
 <?php include dirname(__FILE__).'/footer.php'; ?>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
