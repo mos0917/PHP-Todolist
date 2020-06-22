1771b14cf9cb5e6ac838e9d29fdd7594fb95936d
MOS09 <shonan.fireball@gmail.com>
2020-06-22 18:00:02 +0900

    モーダル内表示位置変更

--- index.php
+++ index.php
@@ -255,7 +255,7 @@ while ($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
                                                 </ul>
                                             </div>
                                         </form>
-                                        <div class="modal-footer>
+                                        <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
                                             <button type="submit" class="btn btn-primary" name="modify" value="modify">更新</button>
                                         </div>
