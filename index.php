308d9c96c996e7ca9061baa8527c1cac4cedad2c
MOS09 <shonan.fireball@gmail.com>
2020-06-22 10:32:34 +0900

    更新処理微修正

--- index.php
+++ index.php
@@ -78,6 +78,43 @@ if (isset($_POST['method']) && ($_POST['method'] === 'put')) {
 
     $dbh = null;
 }
+if ($_POST['modify']) {
+    $editid = $_POST['id'];
+    $editname = $_POST['editname'];
+    $editmemo = $_POST['editmemo'];
+    $editdeaddate = $_POST['editdeadline_date'];
+
+    $editname = htmlspecialchars($name, ENT_QUOTES);
+    $editmemo = htmlspecialchars($memo, ENT_QUOTES);
+
+    if ($editname === '') {
+        $updateerrors['editname'] = 1;
+    }
+
+    if ($editmemo === '') {
+        $updateerrors['editmemo'] = 1;
+    }
+
+    if (count($updateerrors) === 0) {
+        $dbh = db_connect();
+
+        $sql = 'UPDATE tasks SET name = :edittaskname, memo = :edittaskvalue,deadline_date = :editdeadline_date,update_date = :modifydate WHERE id = :editid';
+        $stmt = $dbh->prepare($sql);
+
+        $stmt->bindValue(':edittaskname', $editname, PDO::PARAM_STR);
+        $stmt->bindValue(':edittaskvalue', $editmemo, PDO::PARAM_STR);
+        $stmt->bindValue(':editdeadline_date,', $editdeaddate, PDO::PARAM_STR);
+        $stmt->bindValue(':modifydate', $date, PDO::PARAM_STR);
+        $stmt->bindValue(':editid', $editid, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $dbh = null;
+
+        unset($editname, $editmemo);
+    }
+}
+
 ?>
 
 <!doctype html>
