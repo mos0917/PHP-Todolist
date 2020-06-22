50a68279a9a873d7f0d1d150ebd7920bfc864a9f
MOS09 <shonan.fireball@gmail.com>
2020-06-22 18:19:53 +0900

    モーダル内ボタン位置修正

--- index.php
+++ index.php
@@ -256,8 +256,6 @@ while ($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
                                             </div>
                                         </form>
                                     </div>
-                                </div>
-                                <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
                                     <button type="submit" class="btn btn-primary" name="modify" value="modify">更新</button>
                                 </div>
