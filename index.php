a132ec878fe66c5e5966291414357857c89b9ab1
MOS09 <shonan.fireball@gmail.com>
2020-06-24 10:35:05 +0900

    完了済みタスク表示ボタン設置(処理未実装)

--- index.php
+++ index.php
@@ -159,8 +159,11 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
 
 <div class="container">
     <div class="row">
-        <div class="col-xs-1 col-lg-11">
+        <div class="col-xs-11 col-lg-10">
             <h1>Todo List</h1>
+        </div>
+        <div class="col-xs-1 col-lg-1 col align-self-center">
+            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#completedtask">完了したタスク</button>
         </div>
         <div class="col-xs-1 col-lg-1 col align-self-center">
             <form action="index.php" method="post" onsubmit="return logoutChk()">
