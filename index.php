7cfd2f4059976cfce11db472283a76be144ecebf
MOS09 <shonan.fireball@gmail.com>
2020-06-24 12:04:12 +0900

    完了済みタスク表示ボタン、モーダル位置変更

--- index.php
+++ index.php
@@ -224,7 +224,7 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
 <HR>
 
 <div class="modal fade" id="completedtask" tabindex="-1" role="dialog" aria-labelledby="completedtask" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">完了済みタスク</h5>
