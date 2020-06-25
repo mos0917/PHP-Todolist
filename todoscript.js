6f82343dc20eb3b2dc903990602693f23615cc41
MOS09 <shonan.fireball@gmail.com>
2020-06-25 22:02:19 +0900

    検証用修正

--- todoscript.js
+++ todoscript.js
@@ -16,7 +16,7 @@ function errChk(){ //タスク登録時のエラーチェック
 }
 
 function comptask(){
-    const url = 'https://blooming-ocean-46381.herokuapp.com/login.php';
+    const url = 'https://blooming-ocean-46381.herokuapp.com/register.php';
     fetch(url).then(function(response) {
         return response.text();
     }).then(function(text) {
