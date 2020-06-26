40a7cde78cb5c3c23df0d00f84f70ac019696404
MOS09 <shonan.fireball@gmail.com>
2020-06-26 11:24:21 +0900

    検証用処理追加

--- completetask.php
+++ completetask.php
@@ -1,4 +1,10 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
+
+require_once 'functions.php';
+include_once 'dbconnect.php';
+
+
     $dbh = db_connect();
 
     $compsql = 'SELECT id, name, memo, deadline_date FROM tasks WHERE done = 1 and delete_flg = 0 and email = "'.$email.'" ORDER BY id DESC';
