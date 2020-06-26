b92935856062edf3d4141bf0e3ad4dae55558a59
MOS09 <shonan.fireball@gmail.com>
2020-06-26 16:22:32 +0900

    検証用処理追加

--- completetask.php
+++ completetask.php
@@ -1,17 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 
-require_once 'functions.php';
-include_once 'dbconnect.php';
 
-$compquery = 'SELECT * FROM users WHERE user_id='.$_SESSION['user'].'';
-$compresult = $mysqli->query($compquery);
-
-
-// ユーザー情報の取り出し
-while ($comprow = $compresult->fetch_assoc()) {
-    $compemail = $comprow['email']; //ユーザーidの取り出し
-}
 
 $compsql = 'SELECT id, name, memo, deadline_date FROM tasks WHERE done = 1 and delete_flg = 0 and email = "'.$compemail.'" ORDER BY id DESC';
 $stmt = $dbh->prepare($compsql);
