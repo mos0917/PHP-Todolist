871709e1e387a6891a2c9066dd118bc6a3b1bab0
MOS09 <shonan.fireball@gmail.com>
2020-06-26 12:02:12 +0900

    検証用処理追加

--- completetask.php
+++ completetask.php
@@ -4,8 +4,10 @@ date_default_timezone_set('Asia/Tokyo');
 require_once 'functions.php';
 include_once 'dbconnect.php';
 
+$dbh = db_connect();
 $query = 'SELECT * FROM users WHERE user_id='.$_SESSION['user'].'';
 $result = $mysqli->query($query);
+
 
 // ユーザー情報の取り出し
 while ($row = $result->fetch_assoc()) {
