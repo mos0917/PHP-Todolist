6514318e2ff6d2ad358194252a68f01b583bdfdb
MOS09 <shonan.fireball@gmail.com>
2020-06-26 11:15:27 +0900

    検証用処理追加

--- completetask.php
+++ completetask.php
@@ -1,7 +1,7 @@
 <?php
     $dbh = db_connect();
 
-    $sql = 'SELECT id, name, memo, deadline_date FROM tasks WHERE done = 1 and delete_flg = 0 and email = "'.$email.'" ORDER BY id DESC';
-    $stmt = $dbh->prepare($sql);
+    $compsql = 'SELECT id, name, memo, deadline_date FROM tasks WHERE done = 1 and delete_flg = 0 and email = "'.$email.'" ORDER BY id DESC';
+    $stmt = $dbh->prepare($compsql);
     $stmt->execute();
     $dbh = null;
