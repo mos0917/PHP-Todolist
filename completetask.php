0890dda24705f0af4e9a3a586262125071c80f0a
MOS09 <shonan.fireball@gmail.com>
2020-06-29 17:39:38 +0900

    データ抽出処理修正

--- completetask.php
+++ completetask.php
@@ -1,9 +1,19 @@
 <?php
+session_start();
 
 require_once 'functions.php';
 include_once 'dbconnect.php';
 
 $dbh = db_connect();
+
+$query = 'SELECT * FROM users WHERE user_id='.$_SESSION['user'].'';
+$result = $mysqli->query($query);
+// ユーザー情報の取り出し
+while ($row = $result->fetch_assoc()) {
+    $username = $row['username'];
+    $email = $row['email']; //ユーザーidの取り出し
+}
+
 
 $compsql = 'SELECT id, name, memo, deadline_date FROM tasks WHERE done = 1 and delete_flg = 0 ORDER BY id DESC';
 $stmt = $dbh->prepare($compsql);
