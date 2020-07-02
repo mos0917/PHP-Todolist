5d678e9579ca169114ba3099b99071ffcd37ce2d
MOS09 <shonan.fireball@gmail.com>
2020-07-02 09:05:23 +0900

    bootstrapver変更

--- index.php
+++ index.php
@@ -269,6 +269,7 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
                             </div>
                         </div>
                     </div>
+                    <HR>
                 ';
             }
             ?>
@@ -382,7 +383,7 @@ while ($task = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
 
 <?php include dirname(__FILE__).'/footer.php'; ?>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
