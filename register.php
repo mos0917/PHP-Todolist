32da66a74c47821e6e3b5f9872593a53bf1f9ddb
MOS09 <shonan.fireball@gmail.com>
2020-07-03 20:30:55 +0900

    会員登録画面修正

--- register.php
+++ register.php
@@ -16,7 +16,8 @@ include_once 'dbconnect.php';
 <link rel="stylesheet" href="style.css">
 
 <!-- Bootstrap読み込み（スタイリングのため） -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link href="register_style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
@@ -24,33 +25,33 @@ include_once 'dbconnect.php';
 
 
 <form action="login.php" method="post">
-	<h1>会員登録</h1>
-	<div class="row">
-		<div class="col-xs-12 col-lg-12">
-			<input type="text" class="form-control" name="username" placeholder="ユーザー名" required />
-		</div>
-	</div>
-	<br>
-	<div class="row">
-		<div class="col-xs-12 col-lg-12">
-			<input type="email"  class="form-control" name="email" placeholder="メールアドレス" required />
-		</div>
-	</div>
-	<br>
-	<div class="row">
-		<div class="col-xs-12 col-lg-12">
-			<input type="password" pattern="^([a-zA-Z0-9]{8,})$" class="form-control" name="password" placeholder="パスワード" required />
-			<small id="passwordHelpBlock" class="form-text text-muted">
-  				8-20文字で入力してください。大文字、小文字、数字を組み合わせて入力してください。
-			</small>
-		</div>
-	</div>
-	<br>
+    <div class="text-center mb-4">
+        <h1>会員登録</h1>
+    </div>
+    <div class="form-control">
+            <input type="text" class="form-control" name="username" placeholder="ユーザー名" required />
+    </div>
+    <br>
+    <div class="form-control">
+        <input type="email"  class="form-control" name="email" placeholder="メールアドレス" required />
+    </div>
+    <br>
+    <div class="form-control">
+        <input type="password" pattern="^([a-zA-Z0-9]{8,})$" class="form-control" name="password" placeholder="パスワード" required />
+        <small id="passwordHelpBlock" class="form-text text-muted">
+            8-20文字で入力してください。大文字、小文字、数字を組み合わせて入力してください。
+        </small>
+    </div>
+    <br>
 
-	<button type="submit" class="btn btn-success" name="signup">会員登録する</button>
-	<a href="login.php">ログイン画面に戻る</a>
+    <button type="submit" class="btn btn-lg btn-success btn-block" name="signup">会員登録する</button>
+    <a href="login.php">ログイン画面に戻る</a>
 </form>
 
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
 </body>
 </html>
