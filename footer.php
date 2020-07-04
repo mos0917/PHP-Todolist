99401f21a097a59fa1dccb862ea03ed99c13e3c1
MOS09 <shonan.fireball@gmail.com>
2020-07-04 09:11:16 +0900

    フッター情報追加

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja" class="h-100">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>フッタを画面下部に固定</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  //cssファイルの設定など
+</head>
+
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+        <p class="mt-5 mb-3 text-muted text-center">Copyright &copy; 2019-2020 Mos9 at fukurou All Rights Reserved.</p>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+
+</body>
+</html>
