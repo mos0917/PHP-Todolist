b30ec6a5d2ed0988163fb97f740ab5c7409e7d18
MOS09 <shonan.fireball@gmail.com>
2020-07-04 11:35:54 +0900

    ヘッダー追加

--- index.php
+++ index.php
@@ -157,6 +157,41 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
 </head>
 <body>
 
+<header>
+    <!-- 固定ナビゲーションバー -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">ナビゲーションバー</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false" aria-label="ナビゲーションの切替">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar-header">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">ホーム <span class="sr-only">(現位置)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">リンク</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">無効</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ドロップダウン</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">リンク1</a>
+              <a class="dropdown-item" href="#">リンク2</a>
+              <a class="dropdown-item" href="#">リンク3</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="検索..." aria-label="検索...">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+
 
 <div class="container">
     <div class="row">
