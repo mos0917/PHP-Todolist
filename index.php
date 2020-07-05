a9545f38ef36d7d96b59392b92d7220744f6745a
MOS09 <shonan.fireball@gmail.com>
2020-07-05 15:02:44 +0900

    ヘッダーアイコン修正

--- index.php
+++ index.php
@@ -163,10 +163,7 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
         <a class="py-2" href="index.php">
             <svg xmlns="../../core/img/fukurouimg_white.svg" width="24" height="24"></svg>
         </a>
-            <a class="navbar-brand" href="#">TodoList</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false" aria-label="ナビゲーションの切替">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="navbar-brand">TodoList</a>
             <div class="collapse navbar-collapse" id="navbar-header">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
