68708407da769819863f93019d1eb2b0ab323e68
MOS09 <shonan.fireball@gmail.com>
2020-07-06 17:13:03 +0900

    google認証追加

--- login.php
+++ login.php
@@ -1,4 +1,17 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+$id_token = filter_input(INPUT_POST, 'id_token');
+define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
+
+$client = new Google_Client(['client_id' => CLIENT_ID]);
+$payload = $client->verifyIdToken($id_token);
+if ($payload) {
+    $userid = $payload['sub'];
+}
+//ここまでgoogle OAuth認証
+
 ob_start();
 // ここから、register.phpと同様
 session_start();
@@ -109,14 +122,17 @@ if (isset($_POST['login'])) {
 
         <script>
             function onSignIn(googleUser) {/* google のログイン情報を取得*/
-                var profile = googleUser.getBasicProfile();
-                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                console.log('Name: ' + profile.getName());
-                console.log('Image URL: ' + profile.getImageUrl());
-                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                var id_token = googleUser.getAuthResponse().id_token;
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.onload = function() {
+                    console.log('Signed in as: ' + xhr.responseText);
+                };
+                xhr.send('idtoken=' + id_token);
             }
         </script>
-        
+
 
     </div>
 
