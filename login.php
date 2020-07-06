3a0f1eea2ac27ff0a84e263094470550e7ce0bca
MOS09 <shonan.fireball@gmail.com>
2020-07-06 18:14:10 +0900

    google認証追加

--- login.php
+++ login.php
@@ -122,14 +122,11 @@ if (isset($_POST['login'])) {
 
         <script>
             function onSignIn(googleUser) {/* google のログイン情報を取得*/
-                var id_token = googleUser.getAuthResponse().id_token;
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://blooming-ocean-46381.herokuapp.com/login.php');
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                xhr.onload = function() {
-                    console.log('Signed in as: ' + xhr.responseText);
-                };
-                xhr.send('idtoken=' + id_token);
+                var profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
             }
         </script>
 
