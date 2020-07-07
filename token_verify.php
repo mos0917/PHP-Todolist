bbcf971bb6d2a15b5f30d4a0e120c7b00e8b5133
MOS09 <shonan.fireball@gmail.com>
2020-07-08 08:27:13 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -2,9 +2,7 @@
 session_start();
 require_once 'vendor/autoload.php';
 
-// Get $id_token via HTTPS POST.
 
-//$id_token = filter_input(INPUT_POST, 'id_token'); //Todo:これがnullになっている
 $id_token = filter_input(INPUT_POST, 'id_token');
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 
@@ -12,9 +10,6 @@ $client = new Google_Client(['client_id' => CLIENT_ID]);
 $payload = $client->verifyIdToken($id_token);
 if ($payload) {
     $userid = $payload['sub'];
-    // If request specified a G Suite domain:
-    //$domain = $payload['hd'];
-} else {
-    // Invalid ID token
 }
-//echo json_encode($payload, JSON_PRETTY_PRINT);
+$_SESSION['user'] = true;
+exit;
