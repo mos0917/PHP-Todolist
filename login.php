873ce3a44ee562f8e3fed4e58706bb22046e1197
MOS09 <shonan.fireball@gmail.com>
2020-07-07 17:38:38 +0900

    google認証処理修正

--- login.php
+++ login.php
@@ -111,11 +111,8 @@ if (isset($_POST['login'])) {
     </div>
     <script>
         function onSignIn(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log('token: ' + id_token);
         }
         function signOut() {
             var auth2 = gapi.auth2.getAuthInstance();
