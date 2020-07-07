98d5ab8c4772bd2d541e3bc1cfc644507cef6a31
MOS09 <shonan.fireball@gmail.com>
2020-07-07 13:30:44 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -3,7 +3,7 @@
 require_once 'vendor/autoload.php';
 
 $id_token = filter_input(INPUT_POST, 'id_token');
-define('CLIENT_ID', 'XXXXXX.apps.googleusercontent.com');
+define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]);
 $payload = $client->verifyIdToken($id_token);
