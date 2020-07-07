a0877f80bf1900be7f87a5977dffe712f7cb05ea
MOS09 <shonan.fireball@gmail.com>
2020-07-07 20:07:47 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -3,7 +3,8 @@ require_once 'vendor/autoload.php';
 
 // Get $id_token via HTTPS POST.
 
-$id_token = filter_input(INPUT_POST, 'id_token'); //Todo:これがnullになっている
+//$id_token = filter_input(INPUT_POST, 'id_token'); //Todo:これがnullになっている
+$idToken = filter_input(INPUT_POST, 'id_token');
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]);
