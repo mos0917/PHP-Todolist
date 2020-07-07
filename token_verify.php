8b607c08ea0fd0499f409765681ab6e2dd9c8430
MOS09 <shonan.fireball@gmail.com>
2020-07-07 18:10:33 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -7,6 +7,8 @@ $id_token = filter_input(INPUT_POST, 'id_token');
 
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 $client = new Google_Client(['client_id' => CLIENT_ID]);
+
+$payload = $client->verifyIdToken($id_token);
 if ($payload) {
     $userid = $payload['sub'];
     // If request specified a G Suite domain:
