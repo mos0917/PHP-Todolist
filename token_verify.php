b40448a1f3454a167c94491d1cf00dc7338e1206
MOS09 <shonan.fireball@gmail.com>
2020-07-07 13:07:01 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -1,15 +1,12 @@
 <?php
+
 require_once 'vendor/autoload.php';
 
+$id_token = filter_input(INPUT_POST, 'id_token');
+define('CLIENT_ID', 'XXXXXX.apps.googleusercontent.com');
 
-// Get $id_token via HTTPS POST.
-
-$client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+$client = new Google_Client(['client_id' => CLIENT_ID]);
 $payload = $client->verifyIdToken($id_token);
 if ($payload) {
     $userid = $payload['sub'];
-    // If request specified a G Suite domain:
-    //$domain = $payload['hd'];
-} else {
-  // Invalid ID token
 }
