a695357074fca0f6d4a234065bf81051e8a1b58a
MOS09 <shonan.fireball@gmail.com>
2020-07-07 20:28:35 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -4,11 +4,11 @@ require_once 'vendor/autoload.php';
 // Get $id_token via HTTPS POST.
 
 //$id_token = filter_input(INPUT_POST, 'id_token'); //Todo:これがnullになっている
-$idToken = filter_input(INPUT_POST, 'id_token');
+$id_token = filter_input(INPUT_POST, 'id_token');
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]);
-$payload = $client->verifyIdToken($idToken);
+$payload = $client->verifyIdToken($id_token);
 if ($payload) {
     $userid = $payload['sub'];
     // If request specified a G Suite domain:
