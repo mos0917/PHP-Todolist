23c47939df07e3068b4ec9cfbaa114011c16f169
MOS09 <shonan.fireball@gmail.com>
2020-07-07 10:37:10 +0900

    google認証ボタン処理修正

--- todoscript.js
+++ todoscript.js
@@ -41,19 +41,3 @@ function comptask(){
 document.getElementById("resettaskmodal").onclick = function(){
     document.edittaskform.reset();
 }
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
