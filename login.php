c2c3034a49310c53a1abf6d964b9c9427f1878c1
MOS09 <shonan.fireball@gmail.com>
2020-07-07 11:24:36 +0900

    google認証処理修正

--- login.php
+++ login.php
@@ -111,11 +111,15 @@ if (isset($_POST['login'])) {
     </div>
     <script>
         function onSignIn(googleUser) {
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            var id_token = googleUser.getAuthResponse().id_token;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://blooming-ocean-46381.herokuapp.com/login.php');
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onload = function() {
+            console.log('Signed in as: ' + xhr.responseText);
+            };
+            xhr.send('idtoken=' + id_token);
+        
         }
 
         
