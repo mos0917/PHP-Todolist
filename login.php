04a2db86b2ad073d979b403b0542980d6d869927
MOS09 <shonan.fireball@gmail.com>
2020-07-07 13:56:57 +0900

    google認証処理修正

--- login.php
+++ login.php
@@ -112,6 +112,7 @@ if (isset($_POST['login'])) {
     <script>
         function onSignIn(googleUser) {
             var id_token = googleUser.getAuthResponse().id_token;
+            document.write("id_token");
             var xhr = new XMLHttpRequest();
             xhr.open('POST', 'https://blooming-ocean-46381.herokuapp.com/token_verify.php');
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
