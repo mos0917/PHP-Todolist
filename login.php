c6e3b9ccb488bd3bd9f38751c1ba857fed2c71d7
MOS09 <shonan.fireball@gmail.com>
2020-07-08 10:51:04 +0900

    google認証処理修正

--- login.php
+++ login.php
@@ -116,7 +116,7 @@ if (isset($_POST['login'])) {
             function onSignIn(googleUser) {
             var id_token = googleUser.getAuthResponse().id_token;
             var xhr = new XMLHttpRequest();
-            xhr.open('POST', 'test.php');
+            xhr.open('POST', 'token_verify.php');
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             xhr.onload = function() {
                 console.log('Signed in as: ' + xhr.responseText);
