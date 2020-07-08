914cd032bee0cc05c10bb4832581202bcfe13acf
MOS09 <shonan.fireball@gmail.com>
2020-07-09 07:35:41 +0900

    googl認証ボタン修正

--- login.php
+++ login.php
@@ -103,8 +103,8 @@ if (isset($_POST['login'])) {
             <BR>
             <button type="submit" class="btn btn-lg btn-primary btn-block" name="login">ログインする</button>
             <a href="register.php">会員登録はこちら</a>
-            <div class="g-signin2" data-onsuccess="onSignIn" name="id_token"></div>
-            <a href="#" onclick="signOut();">Sign out</a>
+            <!--<div class="g-signin2" data-onsuccess="onSignIn" name="id_token"></div>
+            <a href="#" onclick="signOut();">Sign out</a> -->
         </form>
             <br>
     </div>
@@ -126,13 +126,6 @@ if (isset($_POST['login'])) {
             xhr.send('idtoken=' + id_token);
         }
 
-
-        function signOut() {
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-                console.log('User signed out.');
-            });
-        }
     </script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- todoscript.js
+++ todoscript.js
@@ -15,6 +15,13 @@ function errChk(){ //タスク登録時のエラーチェック
     }
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
 
 function comptask(){
     var div = document.createElement('div');
