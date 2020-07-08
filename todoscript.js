a968b9b2e4d5367188098770b975020a4e8c11e7
MOS09 <shonan.fireball@gmail.com>
2020-07-08 09:14:23 +0900

    google認証処理修正

--- todoscript.js
+++ todoscript.js
@@ -15,6 +15,25 @@ function errChk(){ //タスク登録時のエラーチェック
     }
 }
 
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'token_berify.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + id_token);
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
 function comptask(){
     var div = document.createElement('div');
 
