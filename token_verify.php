a41439ed8f29d665eb1ecf4290d309a96d5c9992
MOS09 <shonan.fireball@gmail.com>
2020-07-08 15:42:54 +0900

    google認証処理修正

--- token_verify.php
+++ token_verify.php
@@ -7,15 +7,15 @@ require_once 'vendor/autoload.php';
 
 $id_token = filter_input(INPUT_POST, 'id_token');
 
-var_dump($id_token);//id_tokenが表示されるか確認
+var_dump('トークン:', $id_token);//id_tokenが表示されるか確認
 
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
 
 $client = new Google_Client(['client_id' => CLIENT_ID]);
-var_dump($client);
+var_dump('クライアント:', $client);
 
 $payload = $client->verifyIdToken($id_token);
-var_dump($payload);
+var_dump('payload:', $payload);
 if ($payload) {
     $userid = $payload['sub'];
 }
