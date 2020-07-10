f0589d78023a12dd9e3f895fa5516fe60eba7e2a
MOS09 <shonan.fireball@gmail.com>
2020-07-10 16:29:07 +0900

    google認証時画面遷移処理追加

--- token_verify.php
+++ token_verify.php
@@ -2,6 +2,9 @@
 
 session_start();
 require_once 'vendor/autoload.php';
+require_once 'functions.php';
+include_once 'dbconnect.php';
+
 
 $id_token = filter_input(INPUT_POST, 'idtoken');
 define('CLIENT_ID', '375099930470-tebhlghcqj0g78541lm6ge3gre656esr.apps.googleusercontent.com');
@@ -11,4 +14,15 @@ $payload = $client->verifyIdToken($id_token);
 if ($payload) {
     $userid = $payload['sub'];
     var_dump($payload);
+}
+
+$query = 'SELECT email from users';
+$emailreresult = $mysqli->query($query);
+
+if ($emailreresult === $payload['email']) {
+    $_SESSION['user'] = true;
+    header('location: index.php');
+} else {
+    $_SESSION['login'] =true;
+    header('location: googleinforegister.php');
 }
