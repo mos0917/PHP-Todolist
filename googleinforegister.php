0fc67a2aff55583b8799e6e06660c795e3ab405e
MOS09 <shonan.fireball@gmail.com>
2020-07-10 16:26:10 +0900

    googleユーザー情報登録用画面作成

---
+++ googleinforegister.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+if (!$_SESSION['login']) {
+    header('location: login.php');
+}
+?>
+
+
+
+
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=width=device-width, initial-scale=1.0">
+<title>Todo List Member registration</title>
+<link rel="stylesheet" href="style.css">
+
+<!-- Bootstrap読み込み（スタイリングのため） -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link href="register_style.css" rel="stylesheet">
+<link rel="shortcut icon" href="../../core/img/fukurouimg_white.svg">
+</head>
+<body>
+<div class="container">
+
+
+<form method="post" class="form-signup">
+    <div class="text-left mb-4">
+        <h1>会員登録</h1>
+    </div>
+    <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="ユーザー名" required />
+            <label for="inputEmail">ユーザー名</label>
+    </div>
+    <div class="form-group">
+        <input type="email"  class="form-control" name="email" placeholder="メールアドレス" required />
+        <label for="inputEmail">メールアドレス</label>
+    </div>
+    <div class="form-group">
+        <input type="password" pattern="^([a-zA-Z0-9]{8,})$" class="form-control" name="password" placeholder="パスワード" required />
+        <label for="inputEmail">パスワード</label>
+        <small id="passwordHelpBlock" class="form-text text-muted">
+            8-20文字で入力してください。大文字、小文字、数字を組み合わせて入力してください。
+        </small>
+    </div>
+    <br>
+
+    <button type="submit" class="btn btn-lg btn-success btn-block" name="signup">会員登録する</button>
+    <a href="login.php">ログイン画面に戻る</a>
+</form>
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+</body>
+</html>
