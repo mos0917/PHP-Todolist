a67f01f23a6285b71dba5ea2167c01a82199ebdf
MOS09 <shonan.fireball@gmail.com>
2020-07-11 11:39:48 +0900

    google認証処理修正

--- googleinforegister.php
+++ googleinforegister.php
@@ -57,5 +57,7 @@ if (!$_SESSION['login']) {
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+
+<script src="todoscript.js"></script>
 </body>
 </html>
