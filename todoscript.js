3158a58eb81d44eb1ca8e06674a72124e228023c
MOS09 <shonan.fireball@gmail.com>
2020-07-14 13:02:01 +0900

    ログアウト時処理修正

--- todoscript.js
+++ todoscript.js
@@ -30,6 +30,13 @@ function onSignIn(googleUser) {
     window.location.href = 'googleinforegister.php';
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
 
 
 
