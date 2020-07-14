0d9a43979058b456af730f8a1d6d23dd2189a7f9
MOS09 <shonan.fireball@gmail.com>
2020-07-14 14:40:43 +0900

    google認証処理修正

--- logout.php
+++ logout.php
@@ -6,18 +6,7 @@ if (isset($_GET['logout'])) {
     unset($_SESSION['user']);
     header("Location: login.php");
     ?>
-    <script>
-        function signOut() {
-            gapi.load('auth2', function () {
-                gapi.auth2.init().then(function () {
-                    var auth2 = gapi.auth2.getAuthInstance()
-                    auth2.signOut().then(function () {
-                        console.log('User signed out.');
-                    });
-                });
-            });
-        }
-    </script>
+
     <?php
 } else {
     header("Location: login.php");
