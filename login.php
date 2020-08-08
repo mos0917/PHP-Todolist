dc9011914f431c7b443ee0bf91dfc224f65f5143
MOS09 <shonan.fireball@gmail.com>
2020-08-08 09:09:43 +0900

    google認証処理、ログアウト時処理修正

--- login.php
+++ login.php
@@ -81,14 +81,14 @@ if (isset($_POST['login'])) {
         <BR>
         <div class="g-signin2" data-onsuccess="onSignIn" name="id_token"></div>
         <a href="#" onclick="signOut();">Sign out</a>
-        <script>
+        <!--<script>
             function signOut() {
                 var auth2 = gapi.auth2.getAuthInstance();
                 auth2.signOut().then(function () {
                     console.log('User signed out.');
                 });
             }
-        </script>
+        </script> -->
         
     </div>
 
--- logout.php
+++ logout.php
@@ -6,6 +6,14 @@ if (isset($_GET['logout'])) {
     unset($_SESSION['user']);
     header("Location: login.php");
     ?>
+    <script>
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+            });
+        }
+    </script>
 
     <?php
 } else {
