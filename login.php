4e79a1233018a1ed7e0d0923a707fd337b8b2de7
MOS09 <shonan.fireball@gmail.com>
2020-08-08 21:33:01 +0900

    ログイン画面デザイン微修正

--- login.php
+++ login.php
@@ -81,15 +81,6 @@ if (isset($_POST['login'])) {
             <div class="sns_login">
                 <span>または</span>
                 <div class="g-signin2" data-onsuccess="onSignIn" name="id_token"></div><!--googleでログインするときのリンク -->
-                <a href="#" onclick="signOut();">Sign out</a>
-                <script>
-                    function signOut() {
-                        var auth2 = gapi.auth2.getAuthInstance();
-                        auth2.signOut().then(function () {
-                            console.log('User signed out.');
-                        });
-                    }
-                </script>
             </div>
         </form>
 
