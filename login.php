f1f4c9ec2786ce8a24b065222418a2ec7e993b08
MOS09 <shonan.fireball@gmail.com>
2020-08-10 20:10:25 +0900

    ログイン画面デザイン微修正

--- login.php
+++ login.php
@@ -81,21 +81,7 @@ if (isset($_POST['login'])) {
 
             <div class="sns_login">
                 <span>または</span>
-                <div class="g-signin2"></div><!--googleでログインするときのリンク -->
-                <script>
-                    function renderButton() {
-                        gapi.signin2.render('g-signin2', {
-                            'scope': 'profile email',
-                            'width': 390,
-                            'height': 50,
-                            'longtitle': true,
-                            'theme': 'dark',
-                            'onsuccess': onSignIn
-                        });
-                    }
-                </script>
-
-
+                <div class="g-signin2" data-width="390" data-height="50" data-longtitle="true" data-onsuccess="onSignIn" name="id_token" data-theme="dark"></div><!--googleでログインするときのリンク -->
             </div>
         </form>
 
@@ -105,7 +91,6 @@ if (isset($_POST['login'])) {
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 
     <script>
         window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')
