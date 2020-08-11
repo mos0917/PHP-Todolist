e6bf4ff6315d210f71aab7bbdaff19527f10428e
MOS09 <shonan.fireball@gmail.com>
2020-08-11 19:57:20 +0900

    タスク登録画面デザイン微修正

--- index.php
+++ index.php
@@ -407,15 +407,17 @@ $result->close();
                                         </form>
                                     </div>
                                     <div class="modal-footer">
-                                        <div class="col-lg-8 align-self-center text-right">
-                                            <button class="btn btn-btn btn-secondary" type="button" id="resettaskmodal" data-dismiss="modal">閉じる</button>
-                                        </div>
-                                        <div class="col-lg-2 align-self-center text-right">
-                                            <button type="submit" class="btn btn-danger" name="delete" value="delete" >削除</button>
-                                        </div>
-                                        <div class="col-lg-2 align-self-center text-right">
-                                            <button type="submit" class="btn btn-primary" name="modify" value="modify">更新</button>
-                                        </div>
+                                        <form action="index.php" method="post" name="editbutton">
+                                            <div class="col-lg-8 align-self-center text-right">
+                                                <button type="submit" class="btn btn-btn btn-secondary" id="resettaskmodal" data-dismiss="modal">閉じる</button>
+                                            </div>
+                                            <div class="col-lg-2 align-self-center text-right">
+                                                <button type="submit" class="btn btn-danger" name="delete" value="delete" >削除</button>
+                                            </div>
+                                            <div class="col-lg-2 align-self-center text-right">
+                                                <button type="submit" class="btn btn-primary" name="modify" value="modify">更新</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
