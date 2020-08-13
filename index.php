aae713acfffad6a1d6cafad61de561d0a484fe82
MOS09 <shonan.fireball@gmail.com>
2020-08-13 10:36:19 +0900

    タスク登録画面デザイン微修正

--- index.php
+++ index.php
@@ -443,7 +443,13 @@ $result->close();
 <script src="https://apis.google.com/js/platform.js?onload=onLoad" async defer></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
-
+<script>
+    $('.navbar-nav>li>a , .dropdown-menu>a').on('click', function(){
+        if(this.id != 'navbarDropdown'){
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+    </script>
 <script src="todoscript.js"></script>
 </body>
 </html>
