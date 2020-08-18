758bedacc1d8101a7ed6137b5767da5fce3328c3
MOS09 <shonan.fireball@gmail.com>
2020-08-18 11:31:21 +0900

    登録済みタスク表示処理修正

--- index.php
+++ index.php
@@ -332,11 +332,11 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
         echo'<div class="container">
                 <div class="row">
                 <div class="col-xs-12 col-lg-3">
-                    ■タスク名:'.$task['name'].'
+                    ■タスク名:wordwrap('.$task['name'].',10,"<br />",false)
                 </div>
                 <br>
                 <div class="col-xs-12 col-lg-3">
-                    ■内容:'.$task['memo'].'
+                    ■内容:wordwrap('.$task['memo'].',10,"<br />",false)
                 </div>
                 <br>
                 <div class="col-xs-12 col-lg-3">
