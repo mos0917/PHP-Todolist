e1ff1b801101668bbc60758a4589d1be4d7315bc
MOS09 <shonan.fireball@gmail.com>
2020-08-21 09:31:24 +0900

    一部処理の修正

--- todoscript.js
+++ todoscript.js
@@ -36,6 +36,19 @@ function onSignIn(googleUser) {//googleでログインボタンを押下した�
     xhr.send('idtoken=' + id_token);
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+function onLoad() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
+}
+
 function comptask(){
     var div = document.createElement('div');
 
