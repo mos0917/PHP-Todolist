49b177de2c203ddd72279b3fbf86a631285e3c52
MOS09 <shonan.fireball@gmail.com>
2021-01-13 14:24:39 +0900

    firebase使用による一時的なcommit

--- login.php
+++ login.php
@@ -94,13 +94,30 @@ if (isset($_POST['login'])) {
                 // Firebase UIの設定
                 //----------------------------------------------
                 var uiConfig = {
+                    callbacks: {
+                        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                            // User successfully signed in.
+                            // Return type determines whether we continue the redirect automatically
+                            // or whether we leave that to developer to handle.
+                            return true;
+                        },
+                        uiShown: function() {
+                            // The widget is rendered.
+                            // Hide the loader.
+                            document.getElementById('loader').style.display = 'none';
+                        }
+                    },
+                    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
                     signInFlow: 'popup',
-                    // ログイン完了時のリダイレクト先
-                    signInSuccessUrl: '../../index.php',
-
-                    // 利用する認証機能
+                    signInSuccessUrl: 'index.php',
                     signInOptions: [
-                        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+                        // Leave the lines as is for the providers you want to offer your users.
+                        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+                        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                        firebase.auth.PhoneAuthProvider.PROVIDER_ID
                     ],
                 };
 
