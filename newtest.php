8ca9f687530fc37fe19f1c16f671769d3918a1ef
MOS09 <shonan.fireball@gmail.com>
2021-02-01 14:42:02 +0900

    firebase使用による一時的なcommit

--- newtest.php
+++ newtest.php
@@ -26,9 +26,8 @@
         var mailAddress = document.getElementById('mailAddress').value;
         var password = document.getElementById('password').value;
 
-        firebase.auth().createUserWithEmailAndPassword(mailAddress, password)
-            .catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(mailAddress, password).catch(function(error) {
                 alert('登録できません（' + error.message + '）');
-            });
+        });
     });
 </script>
