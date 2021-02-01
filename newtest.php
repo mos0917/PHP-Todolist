bcd81f21280e455d8c1606cfcf3f46c939b2376c
MOS09 <shonan.fireball@gmail.com>
2021-02-01 14:32:43 +0900

    firebase使用による一時的なcommit

--- newtest.php
+++ newtest.php
@@ -9,6 +9,19 @@
         <button id="register">新規登録</button>
 
 <script>
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyCfr-SRsboT6mNPc8RBVZpTnDp64EbQzSA",
+        authDomain: "todo-list-e74b4.firebaseapp.com",
+        projectId: "todo-list-e74b4",
+        storageBucket: "todo-list-e74b4.appspot.com",
+        messagingSenderId: "102176473496",
+        appId: "1:102176473496:web:50a47c0b1e5397b93f6bed",
+        measurementId: "G-YZ31FBSYVB"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
     //新規登録処理
     register.addEventListener('click', function(e) {
         var mailAddress = document.getElementById('mailAddress').value;
