40afa0a5732f89fe9436ddbef8488103839ebd88
MOS09 <shonan.fireball@gmail.com>
2021-02-07 14:07:59 +0900

    firebase使用によるログイン昨日の修正

--- login.php
+++ login.php
@@ -130,9 +130,6 @@ $_SESSION['user'] = $user_id;*/
                     signInOptions: [
                         firebase.auth.EmailAuthProvider.PROVIDER_ID,
                         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                        firebase.auth.GithubAuthProvider.PROVIDER_ID
 
                     ],
                 };
