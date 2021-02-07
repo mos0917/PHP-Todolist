73272fa32b7e5bf0886b33ea03be502e3e19e9b1
MOS09 <shonan.fireball@gmail.com>
2021-02-07 11:07:13 +0900

    firebase使用によるログイン昨日の修正

--- logout.php
+++ logout.php
@@ -1,13 +1,8 @@
 <?php
-/*session_start();
+session_start();
 // logout.php?logoutにアクセスしたユーザーをログアウトする
-if (isset($_GET['logout'])) {
-    session_destroy();
-    unset($_SESSION['user']);
     header("Location: login.php");
-} else {
-    header("Location: login.php");
-}*/
+
 ?>
 <script type="text/javascript">
     firebase.auth().signout().then(function(){
