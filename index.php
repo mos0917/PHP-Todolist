0c8c9c3fcc949d73bc0169b5b61dce091c362c20
MOS09 <shonan.fireball@gmail.com>
2021-02-10 13:20:18 +0900

    firebase使用によるログイン昨日の修正

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'functions.php';
 include_once 'dbconnect.php';
 
 $nowdate = date('Y-m-d H:i:s'); //現在日付の取得
+header('location:login.php');
 
 /*if (!isset($_SESSION['user'])) {
     header('Location: login.php');
