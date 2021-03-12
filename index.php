e63c848885de62f194d0f0a2361b8a5046dafbc8
MOS09 <shonan.fireball@gmail.com>
2021-03-12 13:06:09 +0900

    画面遷移の修正

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once 'functions.php';
 include_once 'dbconnect.php';
 
 $nowdate = date('Y-m-d H:i:s'); //現在日付の取得
-if (isset($_SESSION['user']) != '') {
+if (isset($_SESSION['user']) == '') {
     header('Location: login.php');
 }
 
