c49c55c1b5db71530758e6c80781b64adec6a91b
MOS09 <shonan.fireball@gmail.com>
2021-04-21 14:19:42 +0900

    余分な処理の削除

--- index.php
+++ index.php
@@ -144,6 +144,7 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
 <!doctype html>
 <html>
 <head>
+<script src="todoscript.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <meta charset="utf-8">
@@ -415,6 +416,6 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
 
-    <script src="todoscript.js"></script>
+
 </body>
 </html>
