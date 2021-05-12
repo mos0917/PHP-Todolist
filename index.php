62673710c3cd649c6f90d47f7b23eac695e83b02
MOS09 <shonan.fireball@gmail.com>
2021-05-12 23:02:52 +0900

    bootstrapのVer変更による確認

--- index.php
+++ index.php
@@ -146,6 +146,7 @@ if (isset($_POST['delete'])) { //削除ボタン押下時の処理追加
 <head>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <meta charset="utf-8">
 <meta name="viewport" content="width=width=device-width, initial-scale=1">
 <link href="index_style.css" rel="stylesheet">
