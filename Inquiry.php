548b3fd6368706b9c0462f771c063289337bb28b
MOS09 <shonan.fireball@gmail.com>
2021-05-24 16:07:38 +0900

    メール機能の変更

--- Inquiry.php
+++ Inquiry.php
@@ -2,24 +2,6 @@
 session_start();
 
 
-require 'vendor/autoload.php';
-$email = new SendGridMailMail();
-$email->setFrom("test@example.com", "送信者A");
-$email->setSubject("TestMail漢字");
-$email->addTo("taskmanage706@gmail.com", "受信者B");
-$email->addContent("text/plain", "日本語 English");
-$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "n";
-    print_r($response->headers());
-    print $response->body() . "n";
-} catch (Exception $e) {
-    echo 'Caught exception: ' . $e->getMessage() . "n";
-}
-
-
-
 
 if($_POST['inquirysubmit']){
     mb_language("japanese");
