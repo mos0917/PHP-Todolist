393930e79cc3abe36337746925c47e9572395bab
MOS09 <shonan.fireball@gmail.com>
2021-05-26 10:13:30 +0900

    問い合わせ画面の実装を微修正

--- Inquiry.php
+++ Inquiry.php
@@ -73,6 +73,7 @@ if($_POST['inquirysubmit']){
             </div>
         </nav>
     </header>
+
     <main role="main" class="flex-shrink-0">
 
         <div class="container">
@@ -109,6 +110,12 @@ if($_POST['inquirysubmit']){
 
         </div>
     </main>
+
+    <footer class="footer mt-auto py-3 bg-center">
+        <div class="container px-3">
+            <span class="text-muted">Copyright &copy; 2019-2020 Mos9 at fukurou All Rights Reserved.</span>
+        </div>
+    </footer>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
